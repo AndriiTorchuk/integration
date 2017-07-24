0d1a2ec299ee93ec83041c751aa949f92cef3b75
AndriiTorchuk <andriytv86@gmail.com>
2017-07-24 21:26:44 +0200

    fix err

--- test_calculator.py
+++ test_calculator.py
@@ -2,10 +2,11 @@
 My awesome calculator
 """
 from calculator import calc
+from exersise2 import textValue
 
 assert calc(1, 1) == 2
 
-answer = [1,2,3,4,5,6]
+answer = [1, 2, 3, 4, 5, 6]
 
 assert len(answer) > 5 
 
@@ -14,6 +15,4 @@ text = "quick fox jumps over a lazy dog"
 
 assert "foxe" not in text  
 
-from exersise2 import a
-
-assert len(a)>0
+assert len(textValue) > 0
