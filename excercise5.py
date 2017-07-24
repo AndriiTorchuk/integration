4197109a2f20a012ff61bee8f12871cf899406e0
AndriiTorchuk <andriytv86@gmail.com>
2017-07-24 22:39:40 +0200

    Ex5

---
+++ excercise5.py
@@ -0,0 +1,13 @@
+"""
+ex5
+"""
+one = 1
+two = 2
+num = 3
+text = "One is {0} and two is {1}".format(one, two)
+
+def clice_test(text, num):
+    """
+    slise_text function taskes string parameter `text` and integer `num` 
+    """
+    return text[0:num]
