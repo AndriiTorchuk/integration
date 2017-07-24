7894fd49fa79bba9761a6ba36f403bd20293508c
AndriiTorchuk <andriytv86@gmail.com>
2017-07-24 21:48:44 +0200

    add exersise3

---
+++ exsersise3.py
@@ -0,0 +1,14 @@
+"""
+exersise3
+"""
+i = 6
+
+def myfactorial(i)
+    if i = 0: 
+        return 1
+    else:
+        return i*myfactorial(i-1)
+
+print (myfactorial(i))
+
+# teorema Pifagora
--- test_calculator.py
+++ test_calculator.py
@@ -3,6 +3,8 @@ My awesome calculator
 """
 from calculator import calc
 from exersise2 import textValue
+from exsersise3 import *
+from math import factorial
 
 assert calc(1, 1) == 2
 
@@ -16,3 +18,11 @@ text = "quick fox jumps over a lazy dog"
 assert "foxe" not in text  
 
 assert len(textValue) > 0
+
+i = 10
+
+if myfactorial(i) == factorial(i):
+    print ("it works")
+else:
+    print("It work  bad")
+
