0a6ce58cf65d18e4b5202b5ada95c47b42c9b972
AndriiTorchuk <andriytv86@gmail.com>
2017-07-24 20:51:27 +0200

    add something

--- test_calculator.py
+++ test_calculator.py
@@ -1,3 +1,15 @@
+"""
+My awesome calculator
+"""
 from calculator import calc
 
-assert calc(1,1) == 6
+assert calc(1, 1) == 2
+
+answer = [1,2,3,4,5,6]
+
+assert len(answer) > 5 
+
+
+text = "quick fox jumps over a lazy dog"
+
+assert "foxe" not in text  
